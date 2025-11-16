a4ac94dc12e53f058cf24deed62c5535a86da66c
Saurav09K <sauravkaintura49041@gmail.com>
2025-11-16 20:26:05 +0530

    Update 4112-maximize-expression-of-three-elements.java

--- 4112-maximize-expression-of-three-elements/4112-maximize-expression-of-three-elements.java
+++ 4112-maximize-expression-of-three-elements/4112-maximize-expression-of-three-elements.java
@@ -6,22 +6,22 @@ class Solution {
 
         for(int i=0; i<arr.length; i++)
         {
-            if(arr[i]>a)
+            if(arr[i]>a)  //if element greater than a
             {
-                b=a;
-                a=arr[i];
+                b=a;         //store a's value in b, because a higher value is found than previous higher
+                a=arr[i]; // update that higher value to a
             }
-            else if(arr[i]>b)
+            else if(arr[i]>b) // if its not greater than a, but i can be greater than b 
             {
-                b=arr[i];
+                b=arr[i]; // update the value of b, if it is greater
             }
 
-            if(arr[i]<=c)
+            if(arr[i]<=c) // while iterating also keep track of the smallest element in array
             {
                 c=arr[i];
             }
         }
-        return a+b-c;
+        return a+b-c; // return the maximum obtained expression
     }
 }
 //find maximum and 2nd maximum and smallest in array
