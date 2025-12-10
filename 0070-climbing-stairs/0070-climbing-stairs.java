eb864320e1f5036386ac71a7af8a49fcc8404100
Saurav09K <sauravkaintura49041@gmail.com>
2025-12-10 10:44:23 +0530

    Time: 0 ms (100%), Space: 41.9 MB (53.98%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climb(int n,int dp[])
+    {
+        if(n==0) return 1; // if there is no step then , cost to keep at that place is only one way
+        if(n==1) return 1; // if only one step, then goin to that step only one way
+
+       if(dp[n]!=-1)
+       {
+        return dp[n];
+       }
+        return dp[n]= climb(n-1,dp) + climb(n-2,dp);
+    }
+    public int climbStairs(int n) {
+       int dp[]=new int[n+1];
+      Arrays.fill(dp,-1);
+
+      return climb(n,dp);
+    }
+}
