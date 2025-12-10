71fca59a79fb173b5f50f17dda58228a985a7797
Saurav09K <sauravkaintura49041@gmail.com>
2025-12-10 10:50:47 +0530

    Time: 0 ms (100%), Space: 42.1 MB (16.3%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,32 +1,12 @@
-//recursive
 class Solution {
     public int climbStairs(int n) {
-        if(n==0) return 1;
-        if(n==1) return 1;
-        return climbStairs(n-1) + climbStairs(n-2);
-    }
-}
-
-//memoization
-class Solution {
-    public int climb(int n,int dp[])
-    {
-        if(n==0) return 1; // if there is no step then , cost to keep at that place is only one way
-        if(n==1) return 1; // if only one step, then goin to that step only one way
-
-       if(dp[n]!=-1)
-       {
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2; i<=n; i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
         return dp[n];
-       }
-        return dp[n]= climb(n-1,dp) + climb(n-2,dp);
-    }
-    public int climbStairs(int n) {
-       int dp[]=new int[n+1];
-      Arrays.fill(dp,-1);
-
-      return climb(n,dp);
     }
 }
-
-Tabulation
-    
