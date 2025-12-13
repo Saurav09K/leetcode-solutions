91fecbfbd86db9773c8a334007e4b3396c808ccc
Saurav09K <sauravkaintura49041@gmail.com>
2025-12-13 18:57:12 +0530

    Update 0647-palindromic-substrings.java

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int helper(String s,int i,int j)
+    public int helper(String s,int i,int j)   <----------functionn expands at the center of the string
     {
         int count=0;
       
@@ -7,14 +7,14 @@ class Solution {
         {
             if(s.charAt(i)==s.charAt(j))
             {
-                count++;
+                count++;        <---------count is it palindrome or not
             }
             else
             {
                 return count;
             }
-            i--;
-            j++;
+            i--;  <-------expand to left
+            j++;  <-------expand to right
         }
         return count;
     }
@@ -24,10 +24,10 @@ class Solution {
 
         for(int i=0; i<s.length(); i++)
         {
-            int x=helper(s,i,i);
-            int y=helper(s,i,i+1);
+            int x=helper(s,i,i);   //call for odd length, middle one is center
+            int y=helper(s,i,i+1); //call for even length, two middle center's
 
-            totalCount=totalCount+x+y;
+            totalCount=totalCount+x+y; //add total count
             
         }
         return totalCount;
