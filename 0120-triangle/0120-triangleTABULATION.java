1824c33230bd3855f4f1f316a9a3db172cdcecd4
Saurav09K <sauravkaintura49041@gmail.com>
2025-12-13 11:14:17 +0530

    Update 0120-triangleTABULATION.java

--- 0120-triangle/0120-triangleTABULATION.java
+++ 0120-triangle/0120-triangleTABULATION.java
@@ -25,11 +25,11 @@ class Solution {
             dp[n-1][j]=triangle.get(n-1).get(j);
         }
 
-        for(int i=n-2; i>=0; i--)
+        for(int i=n-2; i>=0; i--) //             <-------------------------loop over n-2 because n-1 is the base case initialization row,
         {
-            for(int j=0; j<triangle.get(i).size(); j++)
+            for(int j=0; j<triangle.get(i).size(); j++)//               <---------------- j from 0 to list of ith row size 
             {
-                dp[i][j]=triangle.get(i).get(j)+Math.min(dp[i+1][j],dp[i+1][j+1]);
+                dp[i][j]=triangle.get(i).get(j)+Math.min(dp[i+1][j],dp[i+1][j+1]); //       <--------------------- if list[i][j] condition from recursion
             }
         }
         return dp[0][0];
