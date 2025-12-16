317386f5848d4c6837bc2dcd7a3c09edd460bbf2
Saurav09K <sauravkaintura49041@gmail.com>
2025-12-16 13:20:48 +0530

    Update 0005-longest-palindromic-substringEXPANSION APPROACH.java

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,47 +1,42 @@
 class Solution {
-    public int helper(String s, int i,int j)
-    {
-        int length=0;
-        while(i>=0 && j<s.length())
-        {
-            if(s.charAt(i)==s.charAt(j))
-            {
-                length=j-i+1;
+    public int helper(String s, int i, int j) {
+        int length = 0; // Default length
+        
+        // Expand while characters match and are within bounds
+        while (i >= 0 && j < s.length()) {
+            if (s.charAt(i) == s.charAt(j)) {
+                length = j - i + 1; // Update length for this valid match
+                i--; // Expand Left
+                j++; // Expand Right
+            } else {
+                break; // Stop if mismatch
             }
-            else
-            {
-                return length;
-            }
-            i--;
-            j++;
         }
         return length;
     }
+
     public String longestPalindrome(String s) {
-        int n=s.length();
-        int max=0;
-        int start=0;
-        for(int i=0; i<n; i++)
-        {
-            int plength_1=helper(s,i,i);
+        if (s == null || s.length() < 1) return "";
+        
+        int start = 0;
+        int maxLen = 0;
 
-            if(plength_1>max)
-            {
-                max=plength_1;
-                start=start = i - (plength_1 - 1) / 2;
+        for (int i = 0; i < s.length(); i++) {
+            // Check both Odd (len1) and Even (len2) centers
+            int len1 = helper(s, i, i);
+            int len2 = helper(s, i, i + 1);
+            
+            // Get the best length found at this center
+            int len = Math.max(len1, len2);
 
+            // If this is the longest palindrome seen so far, update global max
+            if (len > maxLen) {
+                maxLen = len;
+                // Calculate start index based on center 'i' and length 'len'
+                start = i - (len - 1) / 2;
             }
-
-
-            int plength_2=helper(s,i,i+1);
-            if(plength_2>max)
-            {
-                max=plength_2;
-                start=start = i - (plength_2 - 1) / 2;
-
-            }
-           
         }
-         return s.substring(start,start+max);
+        
+        return s.substring(start, start + maxLen);
     }
 }
