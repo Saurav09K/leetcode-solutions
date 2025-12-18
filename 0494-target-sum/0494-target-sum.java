f07852ee647992a6a5c1b47c34fcd80f87061012
Saurav09K <sauravkaintura49041@gmail.com>
2025-12-18 13:31:35 +0530

    Update 0494-target-sum.java

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -14,20 +14,20 @@ class Solution {
             }
         }
 
-        if(target<-1000 || target>1000)
+        if(target<-1000 || target>1000) // to handle the negative target as per the problem 
         {
             return 0;
         }
         
-        int offset=1000+target; // to handle negative indexes for memoization(-1000 <= target <= 1000)
+        int offset=1000+target; // to handle negative indexes for memoization(-1000 <= target <= 1000) otherwise OutOfBound Error
 
         if(dp[n][offset]!=-1)
         {
             return dp[n][offset];
         }
 
-        int plus=f(arr,n-1,target+arr[n-1]);
-        int minus=f(arr,n-1,target-arr[n-1]);
+        int plus=f(arr,n-1,target+arr[n-1]);  //plus number and call
+        int minus=f(arr,n-1,target-arr[n-1]); // minus and call
 
         return dp[n][offset] = plus + minus;
 
@@ -43,3 +43,22 @@ class Solution {
         return f(nums,n,target);
     }
 }
+
+
+/*
+Since the problem constraints say the sum of elements will never exceed 1000, the possible range of sums is -1000 to +1000.
+
+Total numbers from -1000 to +1000 is 2001.
+
+So, [n+1][2001] is the perfect size to hold every possible outcome.
+
+The "Universal Shift" (Simplifying your logic)
+Now that you have a size of 2001, you can ditch that complicated if(target < 0) logic entirely.
+
+Think of it like this: Index 1000 in your array is your new "Zero".
+
+If your current target is -5, store it at 1000 + (-5) = 995.
+
+If your current target is +5, store it at 1000 + (5) = 1005.
+
+*/
