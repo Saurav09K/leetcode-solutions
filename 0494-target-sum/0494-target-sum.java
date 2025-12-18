23f294bddfc57433ccd5ce7dfb78c71db3643a36
Saurav09K <sauravkaintura49041@gmail.com>
2025-12-18 13:27:07 +0530

    Time: 9 ms (57.11%), Space: 47.2 MB (9.48%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,4 +1,5 @@
 class Solution {
+    int dp[][];
     public int f(int arr[],int n,int target)
     {
         if(n==0)
@@ -12,14 +13,33 @@ class Solution {
                 return 0;
             }
         }
+
+        if(target<-1000 || target>1000)
+        {
+            return 0;
+        }
+        
+        int offset=1000+target; // to handle negative indexes for memoization(-1000 <= target <= 1000)
+
+        if(dp[n][offset]!=-1)
+        {
+            return dp[n][offset];
+        }
+
         int plus=f(arr,n-1,target+arr[n-1]);
         int minus=f(arr,n-1,target-arr[n-1]);
 
-        return plus + minus;
+        return dp[n][offset] = plus + minus;
 
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n=nums.length;
+        dp=new int[n+1][1000*2+1];
+
+        for(int a[]: dp)
+        {
+            Arrays.fill(a,-1);
+        }
         return f(nums,n,target);
     }
 }
