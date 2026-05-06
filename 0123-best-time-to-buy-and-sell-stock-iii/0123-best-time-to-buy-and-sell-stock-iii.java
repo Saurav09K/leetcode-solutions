61418585cfa7ff31e861659b16544e4b1756c4db
Saurav Kaintura <sauravkaintura49041@gmail.com>
2026-05-06 08:37:58 +0530

    Time: 52 ms (41.2%), Space: 117.8 MB (30.85%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -29,7 +29,35 @@ class Solution {
         }
     }
     public int maxProfit(int[] prices) {
-        dp=new Integer[prices.length][2][3];
-        return f(prices,0,true,2);
+
+        int dp[][][] = new int[prices.length+1][2][3];
+
+        dp[prices.length][1][0]=0;
+
+        for(int i=prices.length-1; i>=0; i--)
+        {
+           for(int buyState=0; buyState<=1; buyState++)
+           {
+               for(int transaction=1; transaction<=2; transaction++)
+               {
+                    if(buyState==1)
+                    {
+                        int buy=-prices[i] + dp[i+1][0][transaction];
+                        int skip=dp[i+1][1][transaction];
+
+                        dp[i][buyState][transaction] = Math.max(buy,skip); 
+                    }
+                    else
+                    {
+                        int sell = prices[i] + dp[i + 1][1][transaction - 1]; 
+                        int hold = 0 + dp[i + 1][0][transaction];
+                        
+                        dp[i][buyState][transaction] = Math.max(sell, hold);
+                    }
+               }
+           }
+        }
+        // The final answer bubbles all the way up to Day 0, able to buy (1), with 2 transactions.
+        return dp[0][1][2];
     }
 }
