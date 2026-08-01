dfca3eac1ed501caa92714fe6dfe7452b76e2b71
Saurav Kaintura <sauravkaintura49041@gmail.com>
2026-08-01 10:13:31 +0530

    Time: 0 ms (100%), Space: 42.9 MB (41.06%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,15 +1,20 @@
 class Solution {
+    Integer dp[][];
     public int f(int arr[], int i, int j)
     {
         if(i>j) return 0;
 
+        if(dp[i][j]!=null) return dp[i][j];
+
         int pickLeft = arr[i] - f(arr,i+1,j);
         int pickRight = arr[j] - f(arr,i,j-1);
 
-        return Math.max(pickLeft,pickRight);
+        return dp[i][j] = Math.max(pickLeft,pickRight);
     }
     public boolean predictTheWinner(int[] arr) {
         if(arr.length==1) return true;
+        dp=new Integer[arr.length][arr.length];
+
         int scoreDiff =  f(arr,0,arr.length-1);
         return (scoreDiff>=0)? true: false;
     }
